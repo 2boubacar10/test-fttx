c5c3d9abb6c1d34183450fe4680183fb4a705624
Boubacar Diallo <dalandafereya@gmail.com>
2022-04-05 13:16:52 +0000

    Modification de l'url en local

--- src/config/backend-base-url.js
+++ src/config/backend-base-url.js
@@ -1,8 +1,8 @@
 // LOCAL BASE URL
-// var baseUrl = 'http://127.0.0.1:8000'
+ var baseUrl = 'http://127.0.0.1:8000'
 
 // TEST PREPROD BASE URL
-var baseUrl = 'http://fttx-free-test.herokuapp.com'
+//var baseUrl = 'http://fttx-free-test.herokuapp.com'
 
 // PRODUCTION BASE URL
 // var baseUrl = ''
