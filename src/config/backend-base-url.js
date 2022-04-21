1bf3bf8643338349894dd17624e6ba7feb8e7906
Boubacar Diallo <boubacar.diallo@free-partenaires.sn>
2022-04-21 12:29:30 +0000

    fix: rajout du status DONE dans la colonne status_demande_paiement dans la table souscription après un paiement réussi par FREE Money

--- src/config/backend-base-url.js
+++ src/config/backend-base-url.js
@@ -1,8 +1,8 @@
 // LOCAL BASE URL
- var baseUrl = 'http://127.0.0.1:8000'
+//var baseUrl = 'http://127.0.0.1:8000'
 
 // TEST PREPROD BASE URL
-//var baseUrl = 'http://10.0.92.10'
+var baseUrl = 'http://10.0.92.10'
 
 // PRODUCTION BASE URL
 // var baseUrl = ''
